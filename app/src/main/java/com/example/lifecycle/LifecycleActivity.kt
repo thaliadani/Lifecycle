3f1006975cee62724cf388559d3ab68dbf040672
Thalia Danielle <thaliaadani.dio@gmail.com>
2025-11-25 20:37:28 -0300

    usando lifecycle

--- app/src/main/java/com/example/lifecycle/LifecycleActivity.kt
+++ app/src/main/java/com/example/lifecycle/LifecycleActivity.kt
@@ -1,13 +1,22 @@
 package com.example.lifecycle
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 
-class LifecycleActivity : AppCompatActivity(){
+class LifecycleActivity : Activity(), LifecycleOwner{
+    override val lifecycle: Lifecycle{
+        TODO("Not yet implemented")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         lifecycle
     }
+
+    override fun getLifecycle() : Lifecycle {
+        TODO("Not yet implemented")
+    }
+
 }
